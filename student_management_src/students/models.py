ce99ac007b509e0b45a7edd3f8559ec9d6eb808d
ASWIN KUMAR P M <aswinkumarpm3@gmail.com>
2021-10-08 12:50:02 +0530

    models added

--- student_management_src/students/models.py
+++ student_management_src/students/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=50)
+    reporting_teacher = models.CharField(max_length=50)
+#Reporting Teacher - It should be a selectable list.
+
+#StudentMarks
+
+#student forignkey
+#term_selction choice one, two
+#subject
+# Subject -
+# Maths -> Marks
+# Science -> Marks
+# History -> Marks
