739f73a780b8c217749e0c679d75ae843735cde5
ASWIN KUMAR P M <aswinkumarpm3@gmail.com>
2021-10-08 17:19:59 +0530

    urls updated

--- student_management_src/students/views.py
+++ student_management_src/students/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
 
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser
