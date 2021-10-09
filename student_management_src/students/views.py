e3e70194d3a8ab022f09f09796493d9bccca22d8
ASWIN KUMAR P M <aswinkumarpm3@gmail.com>
2021-10-09 16:12:22 +0530

    Removed commented lines

--- student_management_src/students/views.py
+++ student_management_src/students/views.py
@@ -65,56 +65,7 @@ class UpdateStudentMarkAPIView(UpdateAPIView):
 class DeleteStudentMarkAPIView(DestroyAPIView):
     queryset = StudentMark.objects.all()
     serializer_class = StudentMarkSerializer
-#
-#
-#
-# @api_view(['GET', 'POST', 'DELETE'])
-# def student_list(request):
-#     if request.method == 'GET':
-#         students = Student.objects.all()
-#
-#         name = request.GET.get('name', None)
-#         if name is not None:
-#             students = students.filter(name__icontains=name)
-#
-#         students_serializer = StudentSerializer(students, many=True)
-#         return JsonResponse(students_serializer.data, safe=False)
-#
-#     elif request.method == 'POST':
-#         student_data = JSONParser().parse(request)
-#         student_serializer = StudentSerializer(data=student_data)
-#         if student_serializer.is_valid():
-#             student_serializer.save()
-#             return JsonResponse(student_serializer.data, status=status.HTTP_201_CREATED)
-#         return JsonResponse(student_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     elif request.method == 'DELETE':
-#         count = Student.objects.all().delete()
-#         return JsonResponse({'message': '{} Students were deleted successfully!'.format(count[0])},
-#                             status=status.HTTP_204_NO_CONTENT)
-#
-#
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def student_detail(request, pk):
-#     try:
-#         student = Student.objects.get(pk=pk)
-#     except Student.DoesNotExist:
-#         return JsonResponse({'message': 'The Student does not exist'}, status=status.HTTP_404_NOT_FOUND)
-#
-#     if request.method == 'GET':
-#         student_serializer = StudentSerializer(student)
-#         return JsonResponse(student_serializer.data)
-#
-#
-#     elif request.method == 'PUT':
-#         student_data = JSONParser().parse(request)
-#         student_serializer = StudentSerializer(student, data=student_data)
-#         if student_serializer.is_valid():
-#             student_serializer.save()
-#             return JsonResponse(student_serializer.data)
-#         return JsonResponse(student_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     elif request.method == 'DELETE':
-#         student.delete()
-#         return JsonResponse({'message': 'Student was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
 
+    def delete(self, request, *args, **kwargs):
+        self.destroy(request, *args, **kwargs)
+        return JsonResponse({'message': 'StudentMark was deleted successfully!'},status=status.HTTP_204_NO_CONTENT)
