53c279b8bde250a75d2c2fe5125ca5ecd109b2d1
ASWIN KUMAR P M <aswinkumarpm3@gmail.com>
2021-10-09 16:12:37 +0530

    Removed commented lines

--- student_management_src/students/urls.py
+++ student_management_src/students/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import url
 from django.urls import path, include
 from students import views
 urlpatterns = [
-    # url(r'^students_list$', views.student_list),
-    # url(r'^student/(?P<pk>[0-9]+)$', views.student_detail),
 
     path("",views.ListStudentAPIView.as_view(),name="student_list"),
     path("create/", views.CreateStudentAPIView.as_view(),name="student_create"),
