f8cbc804141337ffa4f06a992b953667110b61fb
ASWIN KUMAR P M <aswinkumarpm3@gmail.com>
2021-10-09 16:04:44 +0530

    detail view added

--- student_management_src/students/urls.py
+++ student_management_src/students/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 
     path("",views.ListStudentAPIView.as_view(),name="student_list"),
     path("create/", views.CreateStudentAPIView.as_view(),name="student_create"),
+    path("details/<int:pk>/", views.ShowStudentAPIView.as_view(),name="student_details"),
     path("update/<int:pk>/",views.UpdateStudentAPIView.as_view(),name="update_student"),
     path("delete/<int:pk>/",views.DeleteStudentAPIView.as_view(),name="delete_student"),
 
@@ -14,6 +15,7 @@ urlpatterns = [
 
     path("marks/", views.ListStudentMarkAPIView.as_view(), name="student_marks"),
     path("marks/create/", views.CreateStudentMarkAPIView.as_view(), name="student_marks_create"),
+    path("marks/details/<int:pk>/", views.ShowStudentMarkAPIView.as_view(), name="student_marks_details"),
     path("marks/update/<int:pk>/", views.UpdateStudentMarkAPIView.as_view(), name="update_student_marks"),
     path("marks/delete/<int:pk>/", views.DeleteStudentMarkAPIView.as_view(), name="delete_student_marks")
 ]
--- student_management_src/students/views.py
+++ student_management_src/students/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http.response import JsonResponse
-from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 from rest_framework.response import Response
@@ -36,6 +36,14 @@ class DeleteStudentAPIView(DestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+    def delete(self, request, *args, **kwargs):
+        self.destroy(request, *args, **kwargs)
+        return JsonResponse({'message': 'Student was deleted successfully!'},status=status.HTTP_204_NO_CONTENT)
+
+
+class ShowStudentAPIView(RetrieveAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
 
 class ListStudentMarkAPIView(ListAPIView):
@@ -43,6 +51,10 @@ class ListStudentMarkAPIView(ListAPIView):
     serializer_class = StudentMarkSerializer
 
 class CreateStudentMarkAPIView(CreateAPIView):
+    queryset = StudentMark.objects.all()
+    serializer_class = StudentMarkFormSerializer
+
+class ShowStudentMarkAPIView(RetrieveAPIView):
     queryset = StudentMark.objects.all()
     serializer_class = StudentMarkFormSerializer
 
