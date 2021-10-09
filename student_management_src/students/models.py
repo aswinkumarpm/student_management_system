51db58a4d155de49f8ae8fcfc237f8fe3dc3fb9f
ASWIN KUMAR P M <aswinkumarpm3@gmail.com>
2021-10-09 17:10:59 +0530

    unique true added

--- student_management_src/students/models.py
+++ student_management_src/students/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     age = models.IntegerField()
     gender = models.CharField(max_length=50)
     reporting_teacher = models.CharField(max_length=50)
